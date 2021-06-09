0a43e6e12b7c2aaa1489f5b92435b2ca9dbc39ee
Anna <71269728+AnnaKosAlex@users.noreply.github.com>
2021-06-09 20:45:14 +0300

    Homework to lesson 7

---
+++ src/lesson7/Plate.java
@@ -0,0 +1,30 @@
+package lesson7;
+
+public class Plate {
+    private int food;
+
+    public int getFood() {
+        return food;
+    }
+
+    public void setFood(int food) {
+        this.food = food;
+    }
+
+    public Plate(int food) {
+        this.food = food;
+    }
+
+    public void decreaseFood(int n){
+        food -= n;
+    }
+
+    public void increaseFood(int n){
+        food += n;
+        System.out.println("We put on the plane " + n + "g of food!");
+    }
+
+    public void  info(){
+        System.out.println("plate: " + food);
+    }
+}
