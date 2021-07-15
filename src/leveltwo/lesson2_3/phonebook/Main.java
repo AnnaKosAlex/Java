4d74df6b08a3176519e1c28444d4e6842e221ce6
Anna <71269728+AnnaKosAlex@users.noreply.github.com>
2021-07-15 22:03:07 +0300

    Homework to lesson 2.3

--- src/leveltwo/lesson2_3/phonebook/Main.java
+++ src/leveltwo/lesson2_3/phonebook/Main.java
@@ -7,15 +7,12 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
+        Map<String, Integer> phoneClients = new HashMap<>();
 
-
-        Map<String, List<Integer>> phoneClients = new HashMap<>();
-        List<Integer> numbers = new ArrayList<>();
-        PhoneBook phoneBook = new PhoneBook(phoneClients);
         String s = "Popov";
-        Integer i = 0;
-        PhoneBook.addingClients(phoneClients,numbers);
-        phoneBook.createPhoneBook(phoneClients,numbers, s, i);
+        PhoneBook phoneBook = new PhoneBook();
+
+        PhoneBook.addingClients(phoneClients);
         PhoneBook.findTheClient(phoneClients,s);
     }
 
--- src/leveltwo/lesson2_3/phonebook/PhoneBook.java
+++ src/leveltwo/lesson2_3/phonebook/PhoneBook.java
@@ -4,60 +4,25 @@ import java.util.*;
 
 public class PhoneBook {
 
-    Map<String, List<Integer>> phoneClient = new HashMap<>();
-
-    int number;
-
-    public PhoneBook(Map<String, List<Integer>> phoneClient) {
-        this.phoneClient = phoneClient;
-    }
-
-    public void createPhoneBook(Map<String, List<Integer>> phoneClient, List<Integer> numbers, String s, Integer i) {
-        List<Integer> number = new ArrayList<>();
-
-        if (this.phoneClient.containsKey(s)) {
-            numbers = this.phoneClient.get(s);
-            numbers.add(i);
-            this.phoneClient.put(s, number);
-        } else {
-            numbers.add(i);
-            this.phoneClient.put(s, number);
-        }
-
-        numbers.add(4950386);
-        numbers.add(4951161);
-        numbers.add(4952936);
-        numbers.add(4952123);
-        numbers.add(4953945);
-        numbers.add(4954937);
-        numbers.add(4952947);
-        numbers.add(4953957);
-        numbers.add(4959275);
-        numbers.add(4958965);
+    Map<String, Integer> phoneClient = new HashMap<>();
 
 
-        System.out.println(phoneClient);
-    }
-
-    public static void addingClients(Map<String, List<Integer>> phoneClient, List<Integer> numbers) {
-
-
-        phoneClient.put("Petrov", numbers);
-        phoneClient.put("Popov", numbers);
-        phoneClient.put("Pavlov", numbers);
-        phoneClient.put("Petrova", numbers);
-        phoneClient.put("Kanceva", numbers);
-        phoneClient.put("Lososev", numbers);
-        phoneClient.put("Kostyuk", numbers);
-        phoneClient.put("Ryazancev", numbers);
-        phoneClient.put("Ivanov", numbers);
-        phoneClient.put("Ivanova", numbers);
-
+    public static void addingClients(Map<String, Integer> phoneClient) {
+        phoneClient.put("Petrov", 4958965);
+        phoneClient.put("Popov", 4959275);
+        phoneClient.put("Pavlov", 4953957);
+        phoneClient.put("Petrova", 4952947);
+        phoneClient.put("Kanceva", 4954937);
+        phoneClient.put("Lososev", 4952123);
+        phoneClient.put("Kostyuk", 4952936);
+        phoneClient.put("Ryazancev", 4953945);
+        phoneClient.put("Ivanov", 4951161);
+        phoneClient.put("Ivanova", 4950386);
 
     }
 
 
-    public static String findTheClient(Map<String, List<Integer>> phoneClient, String s) {
+    public static String findTheClient(Map<String, Integer> phoneClient, String s) {
         String res = "";
 
         if (phoneClient.containsKey(s)) {
