f9afe903d7cb1b543b90bdd718125e54797a04b6
Anna <71269728+AnnaKosAlex@users.noreply.github.com>
2021-09-30 13:28:48 +0300

    Homework to lesson 3.1

---
+++ src/levelthree/lesson3_1/Apple.java
@@ -0,0 +1,11 @@
+package levelthree.lesson3_1;
+
+public class Apple extends Fruit {
+
+    private final float weight = 1.0f;
+
+    @Override
+    public float getWeight(){
+        return weight;
+    }
+}
---
+++ src/levelthree/lesson3_1/Orange.java
@@ -0,0 +1,13 @@
+package levelthree.lesson3_1;
+
+public class Orange extends Fruit {
+
+    private final float weight = 1.5f;
+
+
+    @Override
+    public float getWeight() {
+        return weight;
+    }
+
+}
